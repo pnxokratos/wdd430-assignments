35162c1fa4e3840776dd7f1985b8265bec238b48
pnxokratos <her19054@byui.edu>
2023-05-06 00:32:49 +0200

    W03

--- cms/src/app/contacts/contact-detail/contact-detail.component.spec.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {Contact} from '../contact.model';
 import { ContactDetailComponent } from './contact-detail.component';
 
 describe('ContactDetailComponent', () => {
---
+++ cms/src/app/documents/documents.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-documents',
+  templateUrl: './documents.component.html',
+  styleUrls: ['./documents.component.css'],
+})
+export class DocumentsComponent {}
