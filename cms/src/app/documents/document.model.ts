8c48f7cff64f827b66cce87890a641a4fc9d88a0
pnxokratos <her19054@byui.edu>
2023-05-25 00:03:46 +0200

    W05

--- cms/src/app/documents/document.model.ts
+++ cms/src/app/documents/document.model.ts
@@ -1,21 +1,16 @@
 export class Document {
   public id: string;
   public name: string;
-  public description: string;
+  // public description: string;
   public url: string;
-  public children: string;
+  public children: any[];
 
-  constructor(
-    id: string,
-    name: string,
-    description: string,
-    url: string,
-    children: string
-  ) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.url = url;
-    this.children = children;
-  }
+
+ constructor(id: string, name:string, url: string, children: any[]) {
+  this.id = id;
+  this.name = name;
+  // this.description = description;
+  this.url = url;
+  this.children = children;
+ }
 }
