174e3d8c49b133aaf22b0d0905602845c2adac5f
pnxokratos <her19054@byui.edu>
2023-05-25 21:12:05 +0200

    W06

--- cms/src/app/documents/documents.component.ts
+++ cms/src/app/documents/documents.component.ts
@@ -1,26 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Document } from './document.model';
-import { DocumentService } from './document.service';
 
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.css'],
-  providers: [DocumentService]
+  styleUrls: ['./documents.component.css']
 })
-export class DocumentsComponent implements OnInit{
+export class DocumentsComponent {
   selectedDocument: Document;
-
-  constructor(private documentService: DocumentService) {};
-
-  ngOnInit() {
-    this.documentService.documentSelectedEvent
-      .subscribe(
-        (document: Document) => {
-          this.selectedDocument = document;
-        }
-      );
-  }
 
 }
