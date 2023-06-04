a72edb38f989bc71569677989ccc674a33332b1f
pnxokratos <her19054@byui.edu>
2023-06-04 03:36:46 +0200

    W07 completes

--- cms/src/app/contacts/contact.service.ts
+++ cms/src/app/contacts/contact.service.ts
@@ -1,27 +1,113 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import {Contact} from './contact.model';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Contact } from './contact.model';
 import { MOCKCONTACTS } from './MOCKCONTACTS';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactService {
-  contactSelectedEvent = new EventEmitter<Contact>(); 
-  contactChangedEvent = new EventEmitter<Contact[]>();
-
-
-  contacts:Contact[] = [];
+  contactSelectedEvent = new Subject<Contact>();
+  contactChangedEvent = new Subject<Contact[]>();
+  contactListChangedEvent = new Subject<Contact[]>();
+  contacts: Contact[] = [];
+  maxContactId: number;
 
   constructor() {
     this.contacts = MOCKCONTACTS;
-   }
+    this.maxContactId = this.getMaxContactId();
+  }
+
   //method to return the list of contacts
-  getContacts():Contact[] {
+  getContacts(): Contact[] {
     return this.contacts.slice();
   }
 
-
-
   getContact(id: string): Contact {
+    for (let contact of this.contacts) {
+      if (contact.id == id) {
+        return contact;
+      }
+    }
+    return null!;
+  }
+  deleteContact(contact: Contact) {
+    if (!contact) {
+      return;
+    }
+    const pos = this.contacts.indexOf(contact);
+    if (pos < 0) {
+      return;
+    }
+    this.contacts.splice(pos, 1);
+    this.contactChangedEvent.next(this.contacts.slice());
+  }
+
+  getMaxContactId(): number {
+    let maxId = 0;
+    for (const contact of this.contacts) {
+      let currentId = parseInt(contact.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+  }
+
+  addContact(newContact: Contact) {
+    if (!newContact) {
+      return;
+    }
+    this.maxContactId++;
+    newContact.id = this.maxContactId.toString();
+    this.contacts.push(newContact);
+    let contactsListClone = this.contacts.slice();
+    this.contactListChangedEvent.next(contactsListClone);
+  }
+
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if (originalContact === null || newContact === null) {
+      return;
+    }
+    const pos = this.contacts.indexOf(originalContact);
+    if (pos < 0) {
+      return;
+    }
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+    let contactsListClone = this.contacts.slice();
+    this.contactListChangedEvent.next(contactsListClone);
+  }
+}
+
+//  getContact(id:string) {
+
+//   this.contacts.forEach(contact => {
+//     // console.log(contact.id);
+//     // console.log(id);
+//     if(contact.id === id) {
+//       console.log(contact.id);
+//       console.log(id);
+//       console.log(contact.name);
+//       console.log(contact);
+//       return contact
+//     }
+//   })
+//   return null;
+// }
+
+// getContact(id:string): Contact {
+//   for(let contact of this.contacts) {
+//     if(contact.id == id) {
+//       return contact;
+//     } else if (contact == this.contacts[this.contacts.length-1]){
+//       return null;
+//     }
+//   }
+// }
+
+/*
+getContact(id: string): Contact {
     for (const contact of this.contacts) {
       if (contact.id === id) {
         return contact;
@@ -29,50 +115,4 @@ export class ContactService {
     }
     return null!;
   }
-
-  deleteContact(contact: Contact){
-    if(!contact) {
-      return;
-    }
-
-    const pos = this.contacts.indexOf(contact);
-    if (pos < 0) {
-      return;
-    }
-    this.contacts.splice(pos, 1);
-    this.contactChangedEvent.emit(this.contacts.slice());
-  }
-
-
-
- }
-
-
-
-
- //  getContact(id:string) {
-
-  //   this.contacts.forEach(contact => {
-  //     // console.log(contact.id);
-  //     // console.log(id);
-  //     if(contact.id === id) {
-  //       console.log(contact.id);
-  //       console.log(id);
-  //       console.log(contact.name);
-  //       console.log(contact);
-  //       return contact
-  //     }
-  //   })
-  //   return null;
-  // }
-  
-  // getContact(id:string): Contact {
-  //   for(let contact of this.contacts) {
-  //     if(contact.id == id) {
-  //       return contact;
-  //     } else if (contact == this.contacts[this.contacts.length-1]){
-  //       return null;
-  //     }
-  //   }
-
-  // }
+  */
